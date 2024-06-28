9c7468b4a8b80ead9fb570a49e4dd60685d5f2de
Skicken <maks1444@interia.pl>
2024-06-28 18:01:22 +0200

    disabling auto-save formatting, some keybindings and auto-saving files.

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -2,7 +2,20 @@ vim.keymap.set("i", "jj", "<Esc>")
 vim.keymap.set("n", "<F5>", ":!g++ % -o %:r <cr>")
 
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
 vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous [D]iagnostic message" })
 vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next [D]iagnostic message" })
 vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostic [E]rror messages" })
 vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+
+-- buffer navigation
+vim.keymap.set("n", "[b", vim.cmd.bp, { desc = "Go to previous buffer" })
+vim.keymap.set("n", "]b", vim.cmd.bn, { desc = "Go to next buffer" })
+
+vim.keymap.set("n", "[b", vim.cmd.bp, { desc = "Go to previous buffer" })
+vim.keymap.set("n", "]b", vim.cmd.bn, { desc = "Go to next buffer" })
+
+vim.keymap.set("n", "dx", '"_dd', { desc = "delete line" })
+
+vim.keymap.set("n", "[g", "g;", { desc = "Go to previous edit position" })
+vim.keymap.set("n", "]g", "g,", { desc = "Go to next edit position" })
--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -1,38 +1,35 @@
 return {
-	"stevearc/conform.nvim",
-	opts = {},
-	config = function()
-		require("conform").setup({
-			formatters_by_ft = {
-				javascript = { "prettier" },
-				typescript = { "prettier" },
-				javascriptreact = { "prettier" },
-				typescriptreact = { "prettier" },
-				svelte = { "prettier" },
-				css = { "prettier" },
-				html = { "prettier" },
-				json = { "prettier" },
-				yaml = { "prettier" },
-				markdown = { "prettier" },
-				graphql = { "prettier" },
-				lua = { "stylua" },
-				-- Conform will run multiple formatters sequentially
-				python = { "isort", "black" },
-				-- Use a sub-list to run only the first available formatter
-				cpp = { "clang-format" },
-			},
+    "stevearc/conform.nvim",
+    opts = {},
+    config = function()
+        require("conform").setup({
+            formatters_by_ft = {
+                javascript = { "prettier" },
+                typescript = { "prettier" },
+                javascriptreact = { "prettier" },
+                typescriptreact = { "prettier" },
+                svelte = { "prettier" },
+                html = { "prettier" },
+                json = { "prettier" },
+                yaml = { "prettier" },
+                markdown = { "prettier" },
+                graphql = { "prettier" },
+                lua = { "stylua" },
+                -- Conform will run multiple formatters sequentially
+                python = { "isort", "black" },
+                -- Use a sub-list to run only the first available formatter
+                cpp = { "clang-format" },
+            },
 
-			format_on_save = {
-				-- I recommend these options. See :help conform.format for details.
-				lsp_fallback = true,
-				timeout_ms = 1500,
-			},
-		})
-		require("conform").formatters.prettier = {
-			prepend_args = { "--tab-width", "4" },
-		}
-		require("conform").formatters.clang = {
-			prepend_args = { "--tab-width", "4" },
-		}
-	end,
+            vim.keymap.set("n", "<leader>ff", function()
+                vim.lsp.buf.format();
+            end, { desc = "format code" }),
+        })
+        require("conform").formatters.prettier = {
+            prepend_args = { "--tab-width", "4" },
+        }
+        require("conform").formatters.clang = {
+            prepend_args = { "--tab-width", "4" },
+        }
+    end,
 }
