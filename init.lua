9048b8279ee36e9158ab5711b8da86ba3cebf88e
Skicken <maks1444@interia.pl>
2024-07-10 06:41:11 +0200

    moves markdown and lsp plugin to separate directories

--- init.lua
+++ init.lua
@@ -1,2 +1,9 @@
 require("config.init")
-require("lazy").setup("plugins")
+require("lazy").setup({
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+		{ import = "plugins.markdown-preview" },
+		{ import = "plugins.lsp" },
+	},
+})
